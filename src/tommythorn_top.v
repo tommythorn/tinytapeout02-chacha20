82ab3ac5540f2d753062005076e8ef32f5be2293
Tommy Thorn <tommy-github2@thorn.ws>
2022-11-13 11:27:41 -0800

    Personalize

--- src/tommythorn_top.v
+++ src/tommythorn_top.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module seven_segment_seconds #( parameter MAX_COUNT = 1000 ) (
+module tommythorn_top #( parameter MAX_COUNT = 1000 ) (
   input [7:0] io_in,
   output [7:0] io_out
 );
